326c02ace175ce38bea9de8f9a9ef0f32137cb0b
Stanislav Karev <karsv.dvz@gmail.com>
2020-01-17 10:25:26 +0200

    add clear userId at session when logout

--- src/main/java/mate/academy/internetshop/controller/LogoutController.java
+++ src/main/java/mate/academy/internetshop/controller/LogoutController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class LogoutController extends HttpServlet {
     private static final String COOKIE = "MATE";
@@ -13,6 +14,9 @@ public class LogoutController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        session.removeAttribute("userId");
+
         Cookie cookie = new Cookie(COOKIE, null);
         cookie.setMaxAge(0);
         resp.addCookie(cookie);
--- src/main/java/mate/academy/internetshop/web/AuthorizationFilter.java
+++ src/main/java/mate/academy/internetshop/web/AuthorizationFilter.java
@@ -65,7 +65,6 @@ public class AuthorizationFilter implements Filter {
             return;
         } else {
             processDenied(req, resp);
-            return;
         }
     }
 
